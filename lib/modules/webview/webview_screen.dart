d1cf2fe9ddcc6db5f38c366079d78b4d75f6e02e
Osama Gamal <97878002+Osama-Gamal@users.noreply.github.com>
2022-09-15 03:10:52 +0200

    webview page

---
+++ lib/modules/webview/webview_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebviewScreen extends StatelessWidget {
+  String url;
+
+  WebviewScreen({required this.url, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebView(
+        initialUrl: url,
+      ),
+    );
+  }
+}
--- lib/widgets/compontes.dart
+++ lib/widgets/compontes.dart
@@ -1,62 +1,72 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:news_app/modules/webview/webview_screen.dart';
 
 Widget myDivider() => Container(
       height: 0.5,
       color: Colors.grey,
     );
 
-Widget buildArticleItem(article,context) => Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Row(
-        children: [
-          Container(
-            width: 120,
-            height: 120,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              image: DecorationImage(
-                image: NetworkImage(article['urlToImage']),
-                fit: BoxFit.cover,
+Widget buildArticleItem(article, context) => InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => WebviewScreen(
+            url: article['url'],
+          ),
+        ));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                  image: NetworkImage(article['urlToImage']),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            width: 20.0,
-          ),
-          Expanded(
-            child: Container(
-              height: 120.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Text(
-                      article['title'],
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.bodyText1,
+            SizedBox(
+              width: 20.0,
+            ),
+            Expanded(
+              child: Container(
+                height: 120.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        article['title'],
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
                     ),
-                  ),
-                  Text(
-                    article['publishedAt'],
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
+                    Text(
+                      article['publishedAt'],
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
 
-Widget ArticleBuilder(list,context) => ConditionalBuilder(
+Widget ArticleBuilder(list, context) => ConditionalBuilder(
       condition: list.length > 0,
       builder: (context) => ListView.separated(
         physics: BouncingScrollPhysics(),
-        itemBuilder: (context, index) => buildArticleItem(list[index],context),
+        itemBuilder: (context, index) => buildArticleItem(list[index], context),
         separatorBuilder: (context, index) => myDivider(),
         itemCount: 10,
       ),
